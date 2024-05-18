9be8d4f49c64776eec5a87b949f0b85821d54cba
HamzaMateen <hamzamateen1583@gmail.com>
2024-05-19 01:26:01 +0500

    Update the script

--- install-zola.sh
+++ install-zola.sh
@@ -4,7 +4,7 @@
 ZOLA_VERSION="0.18.0"
 
 # download and install Zola 
-wget https://github.com/getzola/zola/releases/download/v0.18.0/zola-v0.18.0-x86_64-unknown-linux-gnu.tar.gz
+curl -sL https://github.com/getzola/zola/releases/download/v0.18.0/zola-v0.18.0-x86_64-unknown-linux-gnu.tar.gz
 tar -xzf zola-v0.18.0-x86_64-unknown-linux-gnu.tar.gz 
 
 mv zola /usr/local/bin/zola 
